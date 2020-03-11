329e66c90a1d9be03cb889e8675b9454adf96be3
iamjigz <jigzimon@gmail.com>
2020-03-12 05:53:58 +0800

    Init material and pwa

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  imports: [
+    ReactiveFormsModule,
+    MaterialModule
+  ],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ]
+})
+export class CoreModule { }
