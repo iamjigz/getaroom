b4c55c8ebb8cefbc274fef164f8b4ca38d5e11a0
iamjigz <jigzimon@gmail.com>
2020-03-31 05:40:22 +0800

    Set tempData for table

--- src/app/trivago/components/trivago-form/trivago-form.component.ts
+++ src/app/trivago/components/trivago-form/trivago-form.component.ts
@@ -16,7 +16,7 @@ export class TrivagoFormComponent implements OnInit {
   constructor(private papa: Papa, public table: TableService) { }
 
   ngOnInit(): void {
-    this.displayedColumns = ['PartnerRef', 'trivagoID', 'POS', 'Base Bid', 'Base Bid Cost'];
+    this.displayedColumns = ['Date', 'PartnerRef', 'trivagoID', 'POS', 'Base Bid'];
   }
 
   private readFiles() {
@@ -42,25 +42,48 @@ export class TrivagoFormComponent implements OnInit {
   readFile(file) {
     const fileData = file.data;
     const fileSize = file.data.size;
+    const matchDate = file.data.name.match(/\d{8}/);
+    const fileDate = matchDate[0];
+    let tempData = [];
     file.inProgress = true;
 
+    console.log(fileDate);
     this.papa.parse(fileData, {
       header: true,
       worker: true,
       skipEmptyLines: true,
-      chunk: (results: ParseResult, parser) => {
+      chunk: (results, parser) => {
         // tslint:disable-next-line: no-string-literal
         const cursor = results.meta['cursor'];
         // tslint:disable-next-line: no-string-literal
         this.table.headers = results.meta['fields'];
-        this.table.data = [...this.table.data, ...results.data];
+        // this.table.data = [...this.table.data, ...results.data];
+
+        tempData = [...tempData, ...results.data];
         file.progress = cursor / fileSize * 100;
       },
       complete: (result, csv) => {
+        this.setDate(tempData, fileDate);
         file.progress = 100;
         file.inProgress = false;
         this.completed++;
       }
     });
+  }
+
+  private async setDate(array, date) {
+    // tslint:disable-next-line: no-string-literal
+    await array.map(row => row['Date'] = date);
+    return this.table.data = [...this.table.data, ...array];
+  }
+
+  count() {
+    const data = this.table.data;
+    // tslint:disable-next-line: no-string-literal
+    const count1 = data.reduce((acc, cur) => cur['Date'] === '20191205' ? acc++ : acc, 0);
+
+    // tslint:disable-next-line: no-string-literal
+    const count2 = data.reduce((acc, cur) => cur['Date'] === '20191206' ? acc++ : acc, 0);
+    console.log(count1, count2);
   }
 }
